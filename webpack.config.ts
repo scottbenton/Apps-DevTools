7906f609861808113d760154bad3678ef75dd9f1
Scott Benton <scott@scottbenton.dev>
2024-02-17 12:19:02 -0500

    feat(libs): Updated libs

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,15 @@
 import { constructModuleWebpackConfig } from "@scottbenton/apps-build";
 import { ModuleScope } from "@scottbenton/apps-config";
 
-const config = constructModuleWebpackConfig({
-  name: ModuleScope.DeveloperTools,
-  dependencies: {},
-  exposes: {
-    "./DevTools": "./src/DevTools.tsx",
+const config = constructModuleWebpackConfig(
+  {
+    name: ModuleScope.DeveloperTools,
+    dependencies: {},
+    exposes: {
+      "./DevTools": "./src/DevTools.tsx",
+    },
   },
-});
+  true
+);
 
 export default config;
